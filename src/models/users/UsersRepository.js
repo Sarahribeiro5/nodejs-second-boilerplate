964dcdc9649d8951a042cfc11a0895c5334c911a
Sarahribeiro5 <158165830+Sarahribeiro5@users.noreply.github.com>
2024-10-31 09:48:54 -0300

    feat: create add method

--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -2,12 +2,20 @@ import User from "./User.js";
 
 class UsersRepository {
     constructor() {
-        this.users = []
+        this.users = [];
     }
 
     getAllUsers() {
         return this.users;
     }
+
+addUser(name, email, password) {
+    const newUser = new User(name, email, password)
+
+    this.users.push(newUser);
+
+    return newUser;
+  }
 }
 
 export default UsersRepository;
